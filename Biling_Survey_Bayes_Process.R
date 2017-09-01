5be46716ca6844fcf7219fc6b0a133973dfaa748
GPSND <31543271+GPSND@users.noreply.github.com>
2017-09-01 11:17:24 -0500

    Update Biling_Survey_Bayes_Process.R

--- Biling_Survey_Bayes_Process.R
+++ Biling_Survey_Bayes_Process.R
@@ -10,17 +10,13 @@ likelihood = function(x) dbinom(55, 100, x)
  text(.7, max(Prior), "Prior", pos = 3)
  axis(1, at = axTicks(1), lab = paste0(axTicks(1)*1e2, "%"))
  
- 
  Likelihood = curve(likelihood, n = 1e4, xlab = "Proportion of \"B\"", ylab = NA, xaxt = "n")$y
  mtext("=", 4, line = 1, cex = 3, font = 1)
  text(.55, max(Likelihood), "Likelihood", pos = 3)
- axis(1, at = c(0, .2, .4, .7, .85, 1), labels = paste0(c(0, .2, .4, .7, .85, 1)*1e2, "%"))
+ axis(1, at = c(0, .2, .4, .7, .85, 1), lab = paste0(c(0, .2, .4, .7, .85, 1)*1e2, "%"))
  axis(1, at = .55, labels = paste0(55, "%"), col = 2, col.axis = 2)
- 
  
  Posterior = curve(posterior, n = 1e4, xlab = "Proportion of \"B\"", ylab = NA, xaxt = "n")$y
  text(.58, max(Posterior), "Posterior", pos = 3)
- axis(1, at = c(0, .2, .4, .7, .85, 1), labels = paste0(c(0, .2, .4, .7, .85, 1)*1e2, "%"))
- axis(1, at = mode, labels = paste0(round(mode, 4)*1e2, "%"), col = 2, col.axis = 2 )
- segments(CI[1], 0, CI[2], 0, lend = 1, lwd = 3, col = "magenta", xpd = NA)
- points(mode, 0, pch = 19, cex = 1.5, col = "magenta", xpd = NA)
+ axis(1, at = c(0, .2, .4, .7, .85, 1), lab = paste0(c(0, .2, .4, .7, .85, 1)*1e2, "%"))
+ axis(1, at = mode, lab = paste0(round(mode, 4)*1e2, "%"), col = 2, col.axis = 2)
