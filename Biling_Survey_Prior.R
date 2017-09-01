a10e93135a410b610173a71d1c3fe3eff5f96cd5
GPSND <31543271+GPSND@users.noreply.github.com>
2017-09-01 14:54:53 -0500

    Update Biling_Survey_Prior.R

--- Biling_Survey_Prior.R
+++ Biling_Survey_Prior.R
@@ -1,6 +1,7 @@
 C = c(15.56689246, 7.051444)
 curve(dbeta(x, C[1], C[2]), n = 1e4, axes = FALSE, lwd = 2, yaxs = "i", xpd = TRUE, 
           xlab = "Proportion of preference for (B)", ylab = NA, font.lab = 2)
+
 axis(1, at = axTicks(1), lab = paste0(axTicks(1)*1e2, "%") )
 axis(1, at = axTicks(1)[4:5], lab = paste0(axTicks(1)[4:5]*1e2, "%"), font = 2)
 x = seq(.6, .8, l = 10)
